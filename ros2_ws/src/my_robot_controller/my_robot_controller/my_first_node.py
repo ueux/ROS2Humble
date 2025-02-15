18d171bd2f9e39a70d531a3c1c48bdaa9ba3b363
ueux <hemenderkuma3000@gmail.com>
2025-02-16 03:14:20 +0530

    Note

--- ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
+++ ros2_ws/src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -6,11 +6,13 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("First_node")
         self.get_logger().info("Hello from Ros2")
+        self.counter_=0
         self.create_timer(1.0,self.timer_callback)
     
     #callback and timer
     def timer_callback(self):
-        self.get_logger().info(f"Hello")
+        self.get_logger().info(f"Hello {self.counter_}")
+        self.counter_+=1
 
 
 def main(args=None):
